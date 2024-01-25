3e6148229866cae3b34ed5114999f6997a89b315
KELVINEITOR458 <kelvinbermeo8@gmail.com>
2024-01-24 20:27:42 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
     console.log("Ingresar a saludar");
+     let cmpImagenSaludo;
+     cmpImagenSaludo = document.getElementById("imgSaludo");
+     cmpImagenSaludo.src = "./imagenes/gif2.gif";
 }
