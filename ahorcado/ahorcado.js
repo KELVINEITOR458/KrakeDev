da57579d6c7190495c8a95aa3de2fb2495b8f8a4
KELVINEITOR458 <kelvinbermeo8@gmail.com>
2024-02-10 16:44:54 -0500

    Paso 1 completado

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -1,4 +1,7 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
+
+
 esMayuscula = function(caracter){
     let cmpCaracter;
     let charCaracter;
@@ -11,3 +14,37 @@ esMayuscula = function(caracter){
         return false;
     }
 }
+
+guardarPalabra = function(){
+    let noHayErrores = true;
+    let cmppalabra;
+    let palabra;
+    cmppalabra = recuperarTexto("txtSecreta");
+    palabra = cmppalabra.length;
+
+    if(palabra < 5){
+        alert("La palabra debe de ser de 5 letras");
+        noHayErrores = false;
+    }else if(palabra > 5){
+        alert("La palabra no debe superar las 5 letras");
+        noHayErrores = false;
+    }
+
+    if(noHayErrores == true){
+        let mayus;
+
+        for(let i=0; i< palabra; i++){
+            mayus= cmppalabra.charCodeAt(i);
+            if(mayus >= 65 && mayus <= 90){
+                palabraSecreta = cmppalabra;
+                console.log(palabraSecreta);
+                noHayErrores = true;
+            }else{
+                alert("La palabra debe ser en mayusculas");
+                noHayErrores = false;
+                break;
+            }
+        } 
+    }
+}
+
