8be49edfa8fe9628b38eef3f919016aa5333e1e5
KELVINEITOR458 <kelvinbermeo8@gmail.com>
2024-02-10 15:31:53 -0500

    se completo el reto 39

--- password/password.js
+++ password/password.js
@@ -4,54 +4,70 @@ validarPassword = function(password){
     cmpPassword = recuperarTexto("txtContra");
     password = cmpPassword.length;
     if(password < 8){
-        mostrarTexto("error1", "La contraseña no puede tener menos de 8 caracteres" );
+        mostrarTexto("errorLong", "La contraseña no puede tener menos de 8 caracteres" );
         noHayErrores = false;
     }else if(password > 16){
-        mostrarTexto("error1", "La contraseña no puede tener más de 16 caracteres" );
+        mostrarTexto("errorLong", "La contraseña no puede tener más de 16 caracteres" );
         noHayErrores = false;
     }else{
+        mostrarTexto("errorLong", "");
         mostrarTexto("error1", "contraseña válida");
         noHayErrores = true;
     }
 
     if(noHayErrores == true){
         let mayus;
-        for(let i=0; i< password.length; i++){
-            mayus= password[i];
-            if(mayus >= "A" && mayus <= "Z"){
-                mostrarTexto("error1", "contraseña válida");
+        for(let i=0; i< password; i++){
+            mayus= cmpPassword.charCodeAt(i);
+            if(mayus >= 65 && mayus <= 90){
+                mostrarTexto("errorMayus", "");
+                noHayErrores = true;
+                break;
             }else{
-                mostrarTexto("error1", "contraseña INVALIDA ")
+                mostrarTexto("errorMayus", "La contraseña debe tener almenos 1 mayúscula");
+                noHayErrores = false;
             }
-        }
+        } 
     }
 
+    if(noHayErrores == true){
+        let mayus;
+        for(let i=0; i< password; i++){
+            mayus= cmpPassword.charCodeAt(i);
+            if(mayus >= 48 && mayus <= 57){
+                mostrarTexto("errorNum", "");
+                noHayErrores = true;
+                break;
+            }else{
+                mostrarTexto("errorNum", "La contraseña debe tener almenos 1 dígito");
+                noHayErrores = false;
+            }
+        } 
+    }
+
+    if(noHayErrores == true){
+        let mayus;
+        for(let i=0; i< password; i++){
+            mayus= cmpPassword.charCodeAt(i);
+            if(mayus == 42 || mayus == 45 || mayus == 95){
+                mostrarTexto("errorCara", "");
+                noHayErrores = true;
+                break;
+            }else{
+                mostrarTexto("errorCara", "La contraseña debe tener almenos 1 caracter, pueden ser (*) (-) (_)");
+                noHayErrores = false;
+            }
+        } 
+    }
+
+
+    if(noHayErrores == true){
+        mostrarTexto("error1", "contraseña válida");
+    }else{
+        mostrarTexto("error1", "contraseña no válida");
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-buscarLetra = function(cadena, letra){
-    let letraIterada;
-    let existeLetra = false;
-    for(let i=0; i<cadena.length; i++){
-        letraIterada = cadena.charAt(i);
-        if(letraIterada == letra){
-            existeLetra = true;
-        }
-    }
-    if(existeLetra ==  true){
-        console.log("existe");
-        return false;
-    }else{
-        console.log("no existe");
-        return true;
-    }
+ejecutarValidacion = function(){
+    validarPassword("txtContra");
 }
